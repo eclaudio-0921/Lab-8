8d8d205fc5436d55d229c3b8c5efa11672758d4c
eclaudio-0921 <143681881+eclaudio-0921@users.noreply.github.com>
2023-11-12 17:20:04 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-function init(Alert Me){
-window.addEventListener("entrybutton");
+function init(){
+  var button = document.getElementById(´entrybutton´);
+
+  function showMeText(){
+    var textbox = document.getElementById(´entryinput´);
+    document.getElementById(´textoutput´).innerHTML = textbox.value;
+    alert(´Eric Claudio: ¨ + textbox.value);
+  }
+
+  button.addEvenListener(´click´, showMeText);
+  }
+
+window.addEventListener(´load´, init);
